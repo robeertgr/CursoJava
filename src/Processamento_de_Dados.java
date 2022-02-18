3f0cc9c0b33fa30b4008ebc5739ebca360119722
Robert Granja <robeertgr@live.com>
2022-02-18 11:56:36 -0300

    Processamento de dados utilizando casting

---
+++ src/Processamento_de_Dados.java
@@ -0,0 +1,29 @@
+public class Processamento_de_Dados {
+    public static void main(String[] args) {
+//        double b, B, h, area;
+//        b = 6.0;
+//        B = 8.0;
+//        h = 5.0;
+//
+//        area = (b + B) / 2.0 * h;
+//        System.out.println(area);
+
+//        int a, b;
+//        double resultado;
+//
+//        a = 5;
+//        b = 2;
+//
+//        resultado = (double) a / b;
+//        System.out.println(resultado);
+
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+
+    }
+}
